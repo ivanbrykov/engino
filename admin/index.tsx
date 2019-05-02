543733efb8d83b41cc240a88b3bf81f3747c219c
Ivan Brykov <ivan.ru@gmail.com>
2019-05-02 09:27:48 -0500

    router

--- admin/index.tsx
+++ admin/index.tsx
@@ -1,25 +1,11 @@
 import * as React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route } from 'react-router-dom';
-import App from './App';
-import { routes, RouteWithSubRoutesProps } from './routes';
+import { BrowserRouter } from 'react-router-dom';
+import { Routes } from './routes';
 
 ReactDOM.render(
   <BrowserRouter basename="/admin">
-    <App />
-    {routes.map((route, i) => (
-      <RouteWithSubRoutes key={i} {...route} />
-    ))}
+    <Routes />
   </BrowserRouter>,
   document.getElementById('root'),
-);
-
-const RouteWithSubRoutes = (route: RouteWithSubRoutesProps) => (
-  <Route
-    path={route.path}
-    render={props => (
-      // pass the sub-routes down to keep nesting
-      <route.component {...props} routes={route.routes} />
-    )}
-  />
 );
---
+++ admin/routes.tsx
@@ -0,0 +1,41 @@
+import * as React from 'react';
+import { Route, RouteProps } from 'react-router-dom';
+import App from './App';
+import Settings from './Settings';
+
+const routes: RouteWithSubRoutesProps[] = [
+  {
+    path: '/',
+    component: App,
+    exact: true,
+  },
+  {
+    path: '/settings',
+    component: Settings,
+  },
+];
+
+export interface RouteWithSubRoutesProps extends RouteProps {
+  routes?: RouteWithSubRoutesProps[];
+}
+
+export const Routes = () => (
+  <>
+    {
+      routes.map((route, i) => (
+        <RouteWithSubRoutes key={i} {...route} />
+      ))
+    }
+  </>
+);
+
+const RouteWithSubRoutes = (route: RouteWithSubRoutesProps) => (
+  <Route
+    path={route.path}
+    exact={route.strict}
+    render={props => (
+      // pass the sub-routes down to keep nesting
+      <route.component {...props} />
+    )}
+  />
+);
